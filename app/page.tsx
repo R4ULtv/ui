d19a5ade0ff31e8f8d690906cb6323ac304ac210
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-04-10 09:35:27 +0200

    Redesign page with two icon picker components

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,51 @@
 "use client";
 
+import CopyUrl from "@/components/copy-url";
 import ThemeSwitch from "@/components/theme-switch";
 import dynamic from "next/dynamic";
 
-const SelectIcon = dynamic(() => import("@/components/icon-picker"), {
+const IconPicker = dynamic(() => import("@/registry/icon-picker/icon-picker"), {
   ssr: false,
 });
 
+const IconPickerVirtualized = dynamic(
+  () => import("@/registry/icon-picker/icon-picker-virtualized"),
+  {
+    ssr: false,
+  },
+);
+
 export default function Page() {
   return (
-    <main className="flex items-center justify-center h-svh">
-      <div className="relative border border-dotted max-w-3xl mx-auto w-full p-4 rounded-xl shadow-md">
-        <div className="absolute top-3 right-3">
-          <ThemeSwitch />
+    <main className="flex items-center justify-center flex-col gap-20 min-h-svh my-12">
+      <div className="max-w-3xl mx-auto w-full">
+        <div className="space-y-1 mb-2 ml-2">
+          <h1 className="font-semibold">Icon Picker</h1>
+          <CopyUrl url="https://ui.raulcarini.dev/r/icon-picker.json" />
         </div>
-        <div className="flex items-center justify-center h-[480px]">
-          <SelectIcon />
+        <div className="relative border border-dotted w-full p-4 rounded-xl shadow-md">
+          <div className="absolute top-3 right-3">
+            <ThemeSwitch />
+          </div>
+          <div className="flex items-center justify-center h-[480px]">
+            <IconPicker />
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-3xl mx-auto w-full">
+        <div className="space-y-1 mb-2 ml-2">
+          <h1 className="font-semibold">Icon Picker with Virtualized Grid</h1>
+          <CopyUrl url="https://ui.raulcarini.dev/r/icon-picker-virtualized.json" />
+        </div>
+
+        <div className="relative border border-dotted w-full p-4 rounded-xl shadow-md">
+          <div className="absolute top-3 right-3">
+            <ThemeSwitch />
+          </div>
+          <div className="flex items-center justify-center h-[480px]">
+            <IconPickerVirtualized />
+          </div>
         </div>
       </div>
     </main>
