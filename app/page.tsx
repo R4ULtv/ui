a5f0aaf0ba97d6ceb06eb2f77f33f08f5ce64e99
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-04-10 11:10:19 +0200

    Add Icon Picker with Popover & Virtualized Grid to home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,13 @@ const IconPickerVirtualized = dynamic(
   },
 );
 
+const IconPickerPopover = dynamic(
+  () => import("@/registry/icon-picker/icon-picker-popover"),
+  {
+    ssr: false,
+  },
+);
+
 export default function Page() {
   return (
     <main className="flex items-center justify-center flex-col gap-20 min-h-svh my-12 p-4">
@@ -45,6 +52,33 @@ export default function Page() {
           </div>
           <div className="flex items-center justify-center h-[480px]">
             <IconPickerVirtualized />
+          </div>
+        </div>
+        <span className="text-xs text-muted-foreground ml-2">
+          Built with{" "}
+          <a
+            target="_blank"
+            href="https://github.com/bvaughn/react-virtualized"
+            className="underline hover:text-foreground"
+          >
+            React Virtualized
+          </a>
+        </span>
+      </div>
+
+      <div className="max-w-3xl mx-auto w-full">
+        <div className="space-y-1 mb-2 ml-2">
+          <h1 className="font-semibold">
+            Icon Picker with Popover & Virtualized Grid
+          </h1>
+          <CopyUrl url="https://ui.raulcarini.dev/r/icon-picker-popover.json" />
+        </div>
+        <div className="relative border border-dotted w-full p-4 rounded-xl shadow-md">
+          <div className="absolute top-3 right-3">
+            <ThemeSwitch />
+          </div>
+          <div className="flex items-center justify-center h-[480px]">
+            <IconPickerPopover />
           </div>
         </div>
         <span className="text-xs text-muted-foreground ml-2">
