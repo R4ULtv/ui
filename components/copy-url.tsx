7c797851dd6603dad134192459c16b10a47e83c3
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-05-01 14:04:12 +0200

    Add components for clipboard copy buttons

---
+++ components/copy-url.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+import { CheckIcon, ClipboardIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function CopyURL({ url }: { url: string }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(url);
+    setCopied(true);
+    toast.success("Registry URL copied to clipboard");
+
+    setTimeout(() => setCopied(false), 2000);
+  }, [url]);
+
+  return (
+    <Button
+      aria-label="Copy URL"
+      variant="outline"
+      size="sm"
+      onClick={handleCopy}
+    >
+      {copied ? <CheckIcon size={16} /> : <ClipboardIcon size={16} />} URL
+    </Button>
+  );
+}
