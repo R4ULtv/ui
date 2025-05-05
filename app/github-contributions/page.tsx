f545a0a8edb7351dd684696e759be57a82b8203d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-05-05 11:20:36 +0200

    Add Github Contributions component page

---
+++ app/github-contributions/page.tsx
@@ -0,0 +1,60 @@
+import V0Icon from "@/components/icons/v0";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+import CopyShadcn from "@/components/copy-shadcn";
+import CopyURL from "@/components/copy-url";
+import { GithubContributionsExample } from "@/components/examples/github-contributions";
+
+const components = [
+  {
+    name: "Github Contributions",
+    description: "A basic github contributions table.",
+    registry: "https://ui.raulcarini.dev/r/github-contributions.json",
+    component: GithubContributionsExample,
+  },
+];
+
+export default function Page() {
+  return (
+    <main className="max-w-6xl mx-auto flex flex-col px-4 py-8 flex-1 gap-8 md:gap-12">
+      {components.map((component) => (
+        <div key={component.name} className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2 md:flex-row items-center justify-between">
+            <div className="flex items-center gap-2">
+              <div className="text-sm line-clamp-1 font-medium">
+                {component.name}
+              </div>
+              <Separator
+                orientation="vertical"
+                className="!h-4 hidden lg:flex"
+              />
+              <div className="text-sm text-muted-foreground line-clamp-1 hidden lg:flex">
+                {component.description}
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <CopyShadcn
+                text={`npx shadcn@latest add ${component.registry}`}
+              />
+              <CopyURL url={component.registry} />
+              <Button variant="default" size="sm" asChild>
+                <a
+                  href={`https://v0.dev/chat/api/open?url=${component.registry}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Open in
+                  <V0Icon />
+                </a>
+              </Button>
+            </div>
+          </div>
+          <div className="flex items-center border rounded-lg justify-center min-h-[400px] p-4 md:p-10 relative bg-muted/30">
+            <component.component />
+          </div>
+        </div>
+      ))}
+    </main>
+  );
+}
