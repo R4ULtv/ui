a0f9798e77dfe5f8b3f322119273cb147928279b
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-05-07 09:42:34 +0200

    Add section IDs and improve text semantics

--- app/icon-picker/page.tsx
+++ app/icon-picker/page.tsx
@@ -87,19 +87,23 @@ export default function Page() {
   return (
     <main className="max-w-6xl mx-auto flex flex-col px-4 py-8 flex-1 gap-8 md:gap-12">
       {components.map((component) => (
-        <div key={component.name} className="flex flex-col gap-4">
+        <div
+          key={component.name}
+          id={component.name.toLowerCase().replace(/ /g, "-")}
+          className="flex flex-col gap-4"
+        >
           <div className="flex flex-col gap-2 md:flex-row items-center justify-between">
             <div className="flex items-center gap-2">
-              <div className="text-sm line-clamp-1 font-medium">
+              <span className="text-sm line-clamp-1 font-medium">
                 {component.name}
-              </div>
+              </span>
               <Separator
                 orientation="vertical"
                 className="!h-4 hidden lg:flex"
               />
-              <div className="text-sm text-muted-foreground line-clamp-1 hidden lg:flex">
+              <span className="text-sm text-muted-foreground line-clamp-1 hidden lg:flex">
                 {component.description}
-              </div>
+              </span>
             </div>
             <div className="flex gap-2">
               <CopyShadcn
