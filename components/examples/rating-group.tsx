afb1c33d24ec87d167cbb92939733844d51d1ab9
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-05-30 11:29:31 +0200

    Change default rating from 0 to 3 in BasicRatingExample

--- components/examples/rating-group.tsx
+++ components/examples/rating-group.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { RatingGroup } from "@/registry/rating-group/rating-group";
 
 export function BasicRatingExample() {
-  const [rating, setRating] = React.useState("0");
+  const [rating, setRating] = React.useState("3");
 
   return <RatingGroup value={rating} onValueChange={setRating} max={5} />;
 }
