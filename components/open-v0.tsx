47d0da9df543e0fac4379553a28ac305f4fd8b3f
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-09-01 08:49:21 +0200

    fix: changed registry param to url

--- components/open-v0.tsx
+++ components/open-v0.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import V0Icon from "@/components/icons/v0";
 
-export default function OpenV0({ registry }: { registry: string }) {
+export default function OpenV0({ url }: { url: string }) {
   return (
     <Button variant="default" size="sm" asChild>
       <a
-        href={`https://v0.dev/chat/api/open?url=${registry}`}
+        href={`https://v0.dev/chat/api/open?url=${url}`}
         target="_blank"
         rel="noreferrer"
       >
